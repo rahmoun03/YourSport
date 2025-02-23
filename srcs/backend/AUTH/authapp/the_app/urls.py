7262f0e32371b3373f79e05ff586c6d6e86b61fd
5tirner <thatfor1337@gmail.com>
2025-02-23 02:30:54 +0100

    Empty work fro now

--- srcs/backend/AUTH/authapp/the_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import check, sign_up
-from rest_framework_simplejwt.views import TokenObtainPairView
-
-urlpatterns = [
-    path('', check),
-    path('signup', sign_up),
-    path('getTokens', TokenObtainPairView.as_view()),
-]
