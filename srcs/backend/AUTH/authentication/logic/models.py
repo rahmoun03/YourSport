af344345a24338c4813f9e22d6c5ff775984a920
5tirner <thatfor1337@gmail.com>
2025-02-24 17:11:39 +0000

    Add Secure login

--- srcs/backend/AUTH/authentication/logic/models.py
+++ srcs/backend/AUTH/authentication/logic/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class auth_db(models.Model):
     email = models.EmailField(unique=True, null=False)
-    password = models.CharField(max_length=1000, null=False)
+    password = models.CharField(unique=True, max_length=10000, null=False)
     activation = models.BooleanField(default=False)
--- srcs/backend/AUTH/authentication/logic/views.py
+++ srcs/backend/AUTH/authentication/logic/views.py
@@ -7,11 +7,11 @@ import bcrypt
 def register(req):
     serial = auth_db_serial(data=req.data)
     if serial.is_valid():
-        serial.validated_data['password'] = bcrypt.hashpw(serial.validated_data['password'].encode('ASCII'), bcrypt.gensalt())
-        print(f"Password Stored on database {serial.validated_data['password']}")
+        hash_pass = bcrypt.hashpw(serial.validated_data['password'].encode('ASCII'), bcrypt.gensalt())
+        serial.validated_data['password'] = hash_pass.decode('ASCII')
         serial.save()
         return response.Response(status=status.HTTP_201_CREATED)
-    return response.Response(status=status.HTTP_400_BAD_REQUEST)
+    return response.Response(serial.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @decorators.api_view(["POST"])
 def login(req):
@@ -22,7 +22,9 @@ def login(req):
     except:
         return response.Response({'error': 'Invalid Email'}, status=status.HTTP_404_NOT_FOUND)
     print(f"Hashed Pass Type: {type(user.password)}")
+    print(f"The password = {user.password}")
     if bcrypt.checkpw(password, user.password.encode('ASCII')):
         return response.Response(
-            { 'active_user': False, 'access_token': "", 'refresh_token': ""}, status=status.HTTP_200_OK)
+            { 'active_user': False, 'access_token': "", 'refresh_token': ""},
+            status=status.HTTP_200_OK)
     return response.Response({'error': 'Invalid Password'}, status=status.HTTP_404_NOT_FOUND)
