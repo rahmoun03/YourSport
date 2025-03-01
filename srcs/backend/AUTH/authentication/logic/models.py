425cf2da05e3dd0a36f5e57999ed0f4cddf42edc
5tirner <thatfor1337@gmail.com>
2025-03-01 17:44:20 +0000

    Add users table

--- srcs/backend/AUTH/authentication/logic/models.py
+++ srcs/backend/AUTH/authentication/logic/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 class auth_db(models.Model):
     email = models.EmailField(unique=True, null=False, primary_key=True)
@@ -9,3 +10,13 @@ class tokens_db(models.Model):
     access_token = models.CharField(max_length=10000, unique=True)
     refresh_token = models.CharField(max_length=10000, unique=True)
     identity = models.EmailField(unique=True, null=False, primary_key=True)
+
+class informations(models.Model):
+    identity = models.EmailField(unique=True, null=False, primary_key=True)
+    username = models.CharField(max_length=50)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    age = models.IntegerField(null=False)
+    create_at = models.DateField(default=datetime.datetime.now())
+    favorite_game = models.CharField(max_length=50)
+    team = models.CharField(max_length=50)
